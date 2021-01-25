23e3e46c1b48035c989c6f46070c2f0e01f90672
adewale2018 <saheedadewaleshittu@gmail.com>
2021-01-24 16:53:18 -0800

    ch: update menu-items to navigate with withRouter HOC

--- src/components/menu-item/MenuItem.js
+++ src/components/menu-item/MenuItem.js
@@ -1,24 +1,29 @@
 import "./MenuItem.scss";
+import { withRouter } from "react-router-dom";
 
-function MenuItem({ title, imageUrl, size }) {
+function MenuItem({ title, imageUrl, size, linkUrl, match, history }) {
   return (
     <>
       <div
         className={`menu-item  d-flex align-items-center flex-grow-1 flex-shrink-1  justify-content-center flex-basis-auto text-muted ${size}`}
+        onClick={() => history.push(`${match.url}${linkUrl}`)}
       >
         <div
           className="background-image"
-          style={{ backgroundImage: `url(${imageUrl})` }}
-        />
+          style={{
+            backgroundImage: `url(${imageUrl})`,
+          }}
+        />{" "}
         <div className="content d-flex flex-column align-items-center justify-content-center">
           <h4 className="title font-weight-bold font-size-h4 text-uppercase text-black-50">
-            {title}
-          </h4>
-          <span className="subtitle font-weight-lighter">SHOP NOW</span>
-        </div>
-      </div>
+            {" "}
+            {title}{" "}
+          </h4>{" "}
+          <span className="subtitle font-weight-lighter"> SHOP NOW </span>{" "}
+        </div>{" "}
+      </div>{" "}
     </>
   );
 }
 
-export default MenuItem;
+export default withRouter(MenuItem);
